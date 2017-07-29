63b15762f459c4db76ae718a600c8fd5ceaa7cd8
90K2 <ilagutin@tickeron.com>
2017-07-29 12:20:41 +0700

    telegram bot controller

---
+++ telegram_bot/tg_sender.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import requests
+from config.cfg import TG_API, TG_TOKEN
+
+
+def main(msg, chat_id, msg_type=""):
+    TOKEN = TG_TOKEN
+    API_URL = TG_API
+
+    emoji = {
+        'critical': "\U0001F198",
+        'ok': "\U00002705"
+    }
+    if msg_type == 'HARD':
+        MESSAGE = emoji['critical'] + msg
+    elif msg_type == 'OK':
+        MESSAGE = emoji['ok'] + msg
+    else:
+        MESSAGE = msg
+    r = requests.get('%s%s/sendMessage?chat_id=%d&text=%s' % (API_URL, TOKEN, int(chat_id), MESSAGE))
+    return r.text
