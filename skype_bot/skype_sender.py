29c620f16d2a235b7e28dbe585108d10e7b85a04
90K2 <ilagutin@tickeron.com>
2017-11-22 01:21:21 +0700

    401 problem fixed

--- skype_bot/skype_sender.py
+++ skype_bot/skype_sender.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python
 import requests
 import json
+from config.cfg import SKYPE_CLIENT_ID, SKYPE_CLIENT_SECRET
 
-CLIENT_ID = 'SKYPE_CLIENT_ID'
-CLIENT_SECRET = 'SKYPE_CLIENT_SECRET'
+CLIENT_ID = SKYPE_CLIENT_ID
+CLIENT_SECRET = SKYPE_CLIENT_SECRET
 
 
 def get_access_token(client_id, client_secret):
     payload = {
         'client_id': client_id,
         'client_secret': client_secret,
-        'scope': 'https://graph.microsoft.com/.default',
+        'scope': 'https://api.botframework.com/.default',
         'grant_type': 'client_credentials'
     }
-    r = requests.post("https://login.microsoftonline.com/common/oauth2/v2.0/token", data=payload)
+    r = requests.post("https://login.microsoftonline.com/botframework.com/oauth2/v2.0/token", data=payload,
+                      headers={'Content-Type': 'application/x-www-form-urlencoded'})
     try:
         response = json.loads(r.text)
         return response['access_token']
@@ -26,7 +28,10 @@ def send_message(token, skype_id, message, type):
         URL = "https://apis.skype.com/v2/conversations/8:{}/activities".format(skype_id)
     if type == 'chat':
         URL = "https://apis.skype.com/v2/conversations/19:{}/activities".format(skype_id)
-    headers = dict(Authorization='Bearer ' + token)
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer %s' % token
+    }
     data = json.dumps(dict(message=dict(content=message))).encode()
     r = requests.post(URL, data=data, headers=headers)
     return r.text, r.status_code
