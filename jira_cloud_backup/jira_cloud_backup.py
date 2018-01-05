a372cb5a2b5fbda6485318838e1819a1cf2e61ff
90K2 <ilagutin@tickeron.com>
2018-01-05 15:32:09 +0700

    updated version for new URLs

--- jira_cloud_backup/jira_cloud_backup.py
+++ jira_cloud_backup/jira_cloud_backup.py
@@ -5,7 +5,7 @@ from requests.auth import HTTPBasicAuth
 
 
 def download_file(url, auth):
-    local_filename = url.split('/')[-1]
+    local_filename = "jira-export_{}.zip".format(time.strftime("%Y-%m-%d", time.gmtime()))
     # NOTE the stream=True parameter
     r = requests.get(url, stream=True, auth=auth)
     with open(local_filename, 'wb') as f:
@@ -17,35 +17,46 @@ def download_file(url, auth):
 
 JIRA_HOST = "company.atlassian.net"
 URL_backup = "https://%(jira_host)s/rest/backup/1/export/runbackup" % {'jira_host': JIRA_HOST}
-URL_download = "https://%(jira_host)s/plugins/servlet/export/download" % {'jira_host': JIRA_HOST}
+URL_download = "https://%(jira_host)s/plugins/servlet" % {'jira_host': JIRA_HOST}
 
 auth = HTTPBasicAuth("_user", "_pass")
 payload = {"cbAttachments": "true", "exportToCloud": "false"}
 headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
 
-backup_status = {}
 
-backup_run = requests.post(URL_backup, data=json.dumps(payload), headers=headers, auth=auth)
-if backup_run.status_code != 200:
-    print(backup_run, backup_run.text)
-else:
-    print('Backup process successfully started')
-    task_id = json.loads(backup_run.text)['taskId']
-    URL_progress = "https://%(jira_host)s/rest/internal/2/task/progress/%(task_id)s" % {'jira_host': JIRA_HOST,
-                                                                                        'task_id': task_id}
-    time.sleep(10)
-    backup_status = {}
-    while "result" not in backup_status.keys():
-        backup_status = json.loads(requests.get(URL_progress, auth=auth).text)
+def main():
+    backup_run = requests.post(URL_backup, data=json.dumps(payload), headers=headers, auth=auth)
+    if backup_run.status_code != 200:
+        print(backup_run, backup_run.text)
+    else:
+        task_id = json.loads(backup_run.text)['taskId']
+        print('Backup process successfully started ; taskid %s' % task_id)
+        URL_progress = "https://%(jira_host)s/rest/backup/1/export/getProgress?taskId=%(task_id)s&_=%(current_ts)s" % \
+                       {'jira_host': JIRA_HOST,
+                        'task_id': task_id,
+                        'current_ts': int(round(time.time() * 1000))
+                        }
+        time.sleep(10)
+        backup_status = {}
+        while "result" not in backup_status.keys():
+            backup_status = json.loads(requests.get(URL_progress, auth=auth).text)
 
-        print("Current status: {} {}; {}".format(backup_status['status'],
-                                                 backup_status['progress'],
-                                                 backup_status['description']
-                                                 )
-              )
-        time.sleep(300)
-print("Downloading backup file")
-result = json.loads(backup_status['result'])
-download_file(URL_download + "/%(mediaFileId)s/%(fileName)s" % {'mediaFileId': result['mediaFileId'],
-                                                                'fileName': result['fileName']},
-              auth)
+            print("Current status: {} {}; {}".format(backup_status['status'],
+                                                     backup_status['progress'],
+                                                     backup_status['description'] + ":" + backup_status['message']
+                                                     )
+                  )
+            time.sleep(120)
+
+        print("Downloading backup file")
+        try:
+            result = backup_status['result']
+            download_file(URL_download + "/%(fileName)s" % {'fileName': result},
+                          auth)
+        except json.decoder.JSONDecodeError as e:
+            print('Something going wrong: %s' % e)
+            print('Last data: %s' % backup_status)
+
+
+if __name__ == '__main__':
+    main()
